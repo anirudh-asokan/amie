d5564b972987038574860d10968f1efaf8b5d6a6
Anirudh Asokan <anirudh.asokan@gmail.com>
2023-06-21 18:24:45 +0530

    Removing debug logs

--- todo_app/models/integration_models.py
+++ todo_app/models/integration_models.py
@@ -6,6 +6,7 @@ from django.contrib.contenttypes.models import ContentType
 
 logger = logging.getLogger(__name__)
 
+
 class AuthToken(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     token = models.CharField(max_length=255)
@@ -13,7 +14,7 @@ class AuthToken(models.Model):
 
 class ThirdPartyApp(models.Model):
     name = models.CharField(max_length=100)
-    service_key = models.CharField(max_length=50) 
+    service_key = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -21,16 +22,18 @@ class ThirdPartyApp(models.Model):
 class ThirdPartyAppIntegration(models.Model):
     """
     Represents the integration of a third-party app with a user's account.
-    
+
     Note:
         Technically, it is possible for a single user to have multiple integrations
         with the same third-party app (e.g., multiple accounts or profiles).
         However, for the purpose of this demo, it is assumed that a user will have
         only one integration with each third-party app.
     """
-    
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='app_integrations')
-    third_party_app = models.ForeignKey(ThirdPartyApp, on_delete=models.CASCADE)
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='app_integrations')
+    third_party_app = models.ForeignKey(
+        ThirdPartyApp, on_delete=models.CASCADE)
     auth_token = models.ForeignKey(AuthToken, on_delete=models.CASCADE)
     last_updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
@@ -40,9 +43,10 @@ class ThirdPartyEntityMapping(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     trackable_entity = GenericForeignKey('content_type', 'object_id')
-    
+
     third_party_id = models.CharField(max_length=255)
-    integration = models.ForeignKey(ThirdPartyAppIntegration, on_delete=models.CASCADE)
+    integration = models.ForeignKey(
+        ThirdPartyAppIntegration, on_delete=models.CASCADE)
     last_synced_at = models.DateTimeField(auto_now=True)
 
     @staticmethod
@@ -51,21 +55,21 @@ class ThirdPartyEntityMapping(models.Model):
             return
         if ThirdPartyEntityMapping.get_trackable_entity(third_party_id, integration):
             return
-        content_type = ContentType.objects.get_for_model(type(trackable_entity))
-        logger.info(f'\n\nMap creating: {trackable_entity.title}, todoist_id {third_party_id}\n\n')
+        content_type = ContentType.objects.get_for_model(
+            type(trackable_entity))
         mapping = ThirdPartyEntityMapping.objects.create(
             content_type=content_type,
             object_id=trackable_entity.pk,
             third_party_id=third_party_id,
             integration=integration
         )
-        logger.info(f'\n\nMap created: {mapping.trackable_entity.title}, todoist_id {mapping.third_party_id}\n\n')
         return mapping
 
     @staticmethod
     def get_third_party_id(trackable_entity, integration):
         try:
-            content_type = ContentType.objects.get_for_model(type(trackable_entity))
+            content_type = ContentType.objects.get_for_model(
+                type(trackable_entity))
             mapping = ThirdPartyEntityMapping.objects.get(
                 content_type=content_type,
                 object_id=trackable_entity.pk,
@@ -75,13 +79,13 @@ class ThirdPartyEntityMapping(models.Model):
         except ThirdPartyEntityMapping.DoesNotExist:
             # If no mapping found, return None
             return None
-        
+
     @staticmethod
     def get_trackable_entity(third_party_id, integration):
         try:
             # Try to get the mapping object from the database
             mapping = ThirdPartyEntityMapping.objects.get(
-                third_party_id=third_party_id, 
+                third_party_id=third_party_id,
                 integration=integration
             )
             return mapping.trackable_entity
